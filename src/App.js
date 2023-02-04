ba98a95a3b726a70a01cd68ab055f937bc23cb73
97970z <97970zz@gmail.com>
2023-02-04 18:17:31 +0900

    connecting test gpt-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { OpenAIApi, Configuration } from "openai";
+import "./App.css";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const configuration = new Configuration({
+    organization: process.env.REACT_APP_ORG,
+    apiKey: process.env.REACT_APP_API,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateResponse = async () => {
+    await openai
+      .createCompletion({
+        prompt: prompt,
+        model: "text-davinci-003",
+        max_tokens: 256,
+        temperature: 0.7,
+      })
+      .then((res) => {
+        setResponse(res.data.choices[0].text);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-main">
+      <h1 className="header">World Best Coolest AI [OpenAI]</h1>
+      <textarea
+        className="app-input"
+        type="text"
+        placeholder="Enter your prompt here"
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+
+      <button onClick={generateResponse}>Generate Response</button>
+      {response && <p className="response-text">{response}</p>}
     </div>
   );
 }
